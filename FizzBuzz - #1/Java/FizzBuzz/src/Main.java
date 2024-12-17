a53c984e1c24fd349b9504f9fbb1edb5a7b265b8
lucithub <lucasvelez2013@gmail.com>
2024-12-17 20:59:32 -0300

    ejercicio 1 java

---
+++ FizzBuzz - #1/Java/FizzBuzz/src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        byte i = 0;
+        while (i < 100){
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
